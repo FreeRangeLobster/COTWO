e0db2f3b327bd8aa07fcbf4c7cc1a2f1d9abf1b5
Juan Vivas <juanvivas@alphrtechnology.co.uk>
2021-07-08 14:27:27 +0100

    Added pcb prototype layout

--- OLEDDisplay/OLEDDisplay/src/main.cpp
+++ OLEDDisplay/OLEDDisplay/src/main.cpp
@@ -16,7 +16,7 @@
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_SSD1306 display((CREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void DisplayUpdatePressureGenerator( float nValue);
 void DisplayUpdatePressureFeed( float nValue);
